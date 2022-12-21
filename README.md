5fb538b90de1e1539f4403df33336bf03f4cb9d1
voho0000 <voho0000@gmail.com>
2022-12-21 09:41:47 +0800

    08-Create Product-and-Rating-Component

--- README.md
+++ README.md
@@ -37,3 +37,7 @@
 7. Add bootstrap UI Framework
     1. npm install react-bootstrap bootstrap
     2. update App.js
+8. Create Product and Rating Component
+    1. create Rating component
+    2. Create Product component
+    3. Use Rating component in Product component
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,10 +1,10 @@
 import { useEffect, useReducer, useState } from 'react';
-import { Link } from 'react-router-dom';
 //import data from '../data';
 import axios from 'axios';
-import Navbar from 'react-bootstrap/Navbar';
-import Container from 'react-bootstrap/Container';
-import { LinkContainer } from 'react-router-bootstrap';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Product from '../components/Product';
+
 
 const reducer = (state, action) => {
     switch (action.type) {
@@ -51,25 +51,16 @@ function HomeScreen() {
                 ) : error ? (
                     <div>Error</div>
                 ) : (
-                    products.map((product) => (
-                    <div className='product' key={product.slug}>
-                        <Link to={`/product/${product.slug}`}>
-                            <img src={product.image} alt={product.name} />
-                        </Link>
-                        <div className='product-info'>
-                            <Link to={`/product/${product.slug}`}>
-                                <p>{product.name}</p>
-                            </Link>
-                            <p>
-                                <strong>${product.price}</strong>
-                            </p>
-                            <button>Add to cart</button>
-                        </div>
-                        </div>
-                    ))
+                    <Row>
+                        {products.map((product) => (
+                            <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
+                                <Product product={product}></Product>
+                            </Col>
+                        ))}
+                    </Row>
                 )}
             </div>
         </div>
-            );
+    );
 }
-            export default HomeScreen;
+export default HomeScreen;
